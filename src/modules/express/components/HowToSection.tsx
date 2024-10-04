dd058c629a0a1f7db7bdb228f353f8f2098386be
Radames Ramirez <60717042+RadamesRamirez0@users.noreply.github.com>
2024-10-04 16:29:27 -0600

    Configurin crowdin

--- src/modules/express/components/HowToSection.tsx
+++ src/modules/express/components/HowToSection.tsx
@@ -19,10 +19,10 @@ const HowToSection = () => {
         <Card className='flex-1'>
           <CardContent className='space-y-6 pt-8 pb-12'>
             <Image
-              src='/svg/satoshi_logo.svg'
-              alt='Satoshi Logo'
-              width={119}
-              height={31}
+              src='/svg/buyStep1.svg'
+              alt='Step 1'
+              width={960}
+              height={540}
               className='w-full'
             />
             <h3 className='text-xl font-bold'>{t(`${orderType}StepOneTitle`)}</h3>
